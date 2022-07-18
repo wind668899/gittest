0b03cf6d968a98255469e4f2f2defa876d992be2
wind668899 <303414103@qq.com>
2022-07-18 17:40:54 +0800

    测试001

--- src/main/java/com/itheima/dao/BookDao.java
+++ src/main/java/com/itheima/dao/BookDao.java
@@ -8,18 +8,18 @@ import java.util.List;
 // TODO 添加@Mapper
 @Mapper
 public interface BookDao {
-    @Insert("insert into tbl_book (type,name,description) values(#{type},#{name},#{description})")
+    @Insert("insert into book (type,name,description) values(#{type},#{name},#{description})")
     public int save(Book book);
 
-    @Update("update tbl_book set type = #{type}, name = #{name}, description = #{description} where id = #{id}")
+    @Update("update book set type = #{type}, name = #{name}, description = #{description} where id = #{id}")
     public int update(Book book);
 
-    @Delete("delete from tbl_book where id = #{id}")
+    @Delete("delete from book where id = #{id}")
     public int delete(Integer id);
 
-    @Select("select * from tbl_book where id = #{id}")
+    @Select("select * from book where id = #{id}")
     public Book getById(Integer id);
 
-    @Select("select * from tbl_book")
+    @Select("select * from book")
     public List<Book> getAll();
 }
